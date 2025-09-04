faad7690ba990635e68b8bba5d95223d17f3b64e
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-05 01:21:20 +0200

    Add Dice container

---
+++ containers/dice/AdapterImplementation.php
@@ -0,0 +1,13 @@
+<?php
+
+use Dice\Dice;
+
+class AdapterImplementation {
+    private Dice $container;
+    public function __construct() {
+        $this->container = new Dice();
+    }
+    public function get(string $class): object {
+        return $this->container->create($class);
+    }
+}
