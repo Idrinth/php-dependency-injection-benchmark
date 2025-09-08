416867642d6b21abd7bf87b0158ad1bcb1b680e8
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-08 19:43:46 +0200

    Rename Zen container to unconfigured and add file-based version (#121)

---
+++ containers/zen.unconfigured/AdapterImplementation.php
@@ -0,0 +1,54 @@
+<?php
+
+use WoohooLabs\Zen\RuntimeContainer;
+use WoohooLabs\Zen\Config\AbstractCompilerConfig;
+use WoohooLabs\Zen\Config\AbstractContainerConfig;
+use WoohooLabs\Zen\Config\EntryPoint\ClassEntryPoint;
+use WoohooLabs\Zen\Config\FileBasedDefinition\FileBasedDefinitionConfig;
+use WoohooLabs\Zen\Config\FileBasedDefinition\FileBasedDefinitionConfigInterface;
+use WoohooLabs\Zen\Config\Preload\PreloadConfig;
+use WoohooLabs\Zen\Config\Preload\PreloadConfigInterface;
+
+class AdapterImplementation {
+    private RuntimeContainer $container;
+    public function __construct() {
+        $this->container = new RuntimeContainer(new CompilerConfig());
+    }
+    public function get(string $class): object {
+        return $this->container->get($class);
+    }
+}
+
+class CompilerConfig extends AbstractCompilerConfig {
+    public function getContainerNamespace(): string {
+        return '';
+    }
+    public function getContainerClassName(): string {
+        return 'GeneratedContainer';
+    }
+    public function useConstructorInjection(): bool {
+        return true;
+    }
+    public function usePropertyInjection(): bool {
+        return false;
+    }
+    public function getPreloadConfig(): PreloadConfigInterface {
+        return PreloadConfig::disabled();
+    }
+    public function getFileBasedDefinitionConfig(): FileBasedDefinitionConfigInterface {
+        return FileBasedDefinitionConfig::disabledGlobally();
+    }
+    public function getContainerConfigs(): array {
+        return [new ContainerConfig()];
+    }
+}
+
+class ContainerConfig extends AbstractContainerConfig {
+    protected function getEntryPoints(): array {
+        return [
+            ClassEntryPoint::create(F06::class),
+            ClassEntryPoint::create(P16::class),
+            ClassEntryPoint::create(Z26::class),
+        ];
+    }
+}
