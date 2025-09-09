cb2dff57ff5857c8b55a409936c6fe5a03e2fba7
Björn Büttner <Idrinth@users.noreply.github.com>
2025-09-09 21:57:59 +0200

    Add unconfigured Ray.Di container

---
+++ containers/ray-di.unconfigured/AdapterImplementation.php
@@ -0,0 +1,14 @@
+<?php
+
+use Ray\Di\Injector;
+
+class AdapterImplementation {
+    private Injector $injector;
+    public function __construct() {
+        $this->injector = new Injector();
+    }
+    public function get(string $class): object {
+        return $this->injector->getInstance($class);
+    }
+}
+
